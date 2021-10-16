7635186256216b9a3d4c4dff1b7da7f1a9734dd2
umeshramya <umeshbilagi@gmail.com>
2021-10-16 16:38:23 +0530

    done

--- src/classes/patient.ts
+++ src/classes/patient.ts
@@ -1,67 +1,46 @@
+import moment from "moment";
 import request from "request";
 import { v4 as uuidv4 } from 'uuid';
 
-export default class Patient{
-    private baseUrl:string;
-    constructor(_baseUrl:string){
-        this.baseUrl=_baseUrl
+export default class Patient {
+    private baseUrl: string;
+    constructor(_baseUrl: string) {
+        this.baseUrl = _baseUrl
     }
 
-    hipVerifyPatinetByHealthId = async (accessToken:string, healthId:string, hipId:string, hipType:string):Promise<any>=>{
+    hipVerifyPatinetByHealthId = async (accessToken: string, healthId: string, hipId: string, hipType: string): Promise<any> => {
         const url = `${this.baseUrl}gateway/v0.5/users/auth/fetch-modes`;
-        console.log(accessToken)
-        
+
+
         const headers = {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${accessToken}`
         }
 
-        console.log(new Date().toISOString())
-        const body = JSON.stringify(
-            {
-                "requestId": "5f7a535d-a3fd-416b-b069-c97d021fbacd",
-                "timestamp": "2021-10-15T18:19:24.172Z",
-                "query": {
-                  "id": "hinapatel79@ndhm",
-                  "purpose": "LINK",
-                  "requester": {
-                    "type": "HIP",
-                    "id": "100005"
-                  }
+
+        const requestBody = {
+            "requestId": uuidv4(),
+            "timestamp": moment.utc().format(`YYYY-MM-DDTHH:mm:ss.SSSSSS`),
+            "query": {
+                "id": healthId,
+                "purpose": "LINK",
+                "requester": {
+                    "type": hipType,
+                    "id": hipId
                 }
-              }
+
+            }
+        }
+        console.log(requestBody)
+        const body = JSON.stringify(requestBody)
+        console.log(body)
 
 
-            // {
-            //     "requestId": `"${uuidv4()}"`,
-            //     "timestamp": `"${new Date().toISOString()}"`,
-            //     "query": {
-            //       "id": `"${healthId}"`,
-            //       "purpose": "LINK",
-            //       "requester": {
-            //         "type": `"${hipType}"`,
-            //         "id": `"${hipId}"`
-            //       }
-            //     }
-            //   }
-        //     {
-        //     "requestId": `${uuidv4()}`,
-        //     "timestamp": `${}`,
-        //     "query": {
-        //         "id": healthId,
-        //         "purpose": "KYC_AND_LINK",
-        //         "requester": {
-        //             "type": hipType,
-        //             "id": hipId
-        //         }
-        //     }
-        // }
-        )
 
         return new Promise((resolve, reject) => {
             request(url, {
                 headers: headers,
-                body: body,
+                body: JSON.parse(JSON.stringify(requestBody)),
                 method: "POST"
             },
                 (err, res) => {
--- test/index.js
+++ test/index.js
@@ -1,24 +1,24 @@
-const {Register, Patient} = require("ndhm-hpr")
+const { Register, Patient } = require("ndhm-hpr")
 const dotenv = require("dotenv").config()
 
-const register = async ()=>{
+const register = async () => {
     const baseUrl = "https://dev.abdm.gov.in/"
-    const endpointUrl="https://webhook.site/38af130b-c175-49f5-8d58-7cae7b3865cd"
+    const endpointUrl = "https://webhook.site/1cb38974-6b4e-490a-8b20-052744f93e91"
     const register = new Register(process.env.CLIENT_ID, process.env.CLIENT_SECRET, baseUrl);
 
-    const accesstoken =(await register.getAccessToken().then(res=>JSON.parse(res))).accessToken
-    await register.updateHealthcareUrl(accesstoken, endpointUrl)
-    await register.registerFacility(accesstoken,
-        {
-            "active" : true,
-            "alias" : ["Eg"],
-            "id" : "jjh_123",
-            "name" : "JJH",
-            "type" : "HIP"
-        })
+    const accesstoken = (await register.getAccessToken().then(res => JSON.parse(res))).accessToken
+    // await register.updateHealthcareUrl(accesstoken, endpointUrl)
+    // await register.registerFacility(accesstoken,
+    //     {
+    //         "active": true,
+    //         "alias": ["Eg"],
+    //         "id": "jjh_123",
+    //         "name": "JJH",
+    //         "type": "HIP"
+    //     })
 
     let patient = new Patient("https://dev.ndhm.gov.in/")
-    await patient.hipVerifyPatinetByHealthId(accesstoken, " umeshbilagi@sbx", "jjh_123", "HRP")
+    await patient.hipVerifyPatinetByHealthId(accesstoken, "umeshbilagi@sbx", "jjh_123", "HIP")
 }
 
 register();
