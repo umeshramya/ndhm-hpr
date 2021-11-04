c783edc366254652d8e5b710c9d4cc638dddc461
Umesh Bilagi <umeshbilagi@gmail.com>
2021-11-04 23:24:57 +0530

    done

--- src/classes/header.ts
+++ src/classes/header.ts
@@ -1,12 +1,19 @@
 export default class Header {
     protected baseUrl: string;
     protected accessToken: string;
-    protected xCmId: "sbx" | "ndhm";
-    protected headers: {}
-    constructor(_baseUrl: string, _accessToken: string, _xCmId: "sbx" | "ndhm") {
+    protected xCmId: string;
+    protected headers={}
+    constructor(_baseUrl: string, _accessToken: string, _xCmId: string) {
         this.baseUrl = _baseUrl;
         this.accessToken = _accessToken;
         this.xCmId = _xCmId;
+        this.setHeader()
+    }
+    /**
+     * header is reset from class wide variables set 
+     * i.e accessToken and xCmId:
+     */
+    setHeader (){
         this.headers = {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${this.accessToken}`,
