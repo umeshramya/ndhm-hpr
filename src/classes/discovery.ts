313bcf8a2bbe26190dbd724ec2493927775c5dfb
umeshramya <umeshbilagi@gmail.com>
2022-02-17 19:20:42 +0530

    v2.1.3

--- src/classes/discovery.ts
+++ src/classes/discovery.ts
@@ -2,49 +2,49 @@ import Header from "./header";
 import { v4 as uuidv4 } from 'uuid';
 import Request from "./request";
 
-export default class Discovery extends Header{
-    constructor(_baseUrl: string, _accessToken: string) {
-        super(_baseUrl, _accessToken)
+export default class Discovery extends Header {
+  constructor(_baseUrl: string, _accessToken: string) {
+    super(_baseUrl, _accessToken)
+  }
+
+  onDiscovery = async (config: {
+    transactionId: string,
+    patientReferenceNumber: string;
+    patientDisplay: string;
+    careContexts: { "referenceNumber": string, "display": string }[],
+    matchedBy: string[]
+    errCode: string;
+    errMessage: string;
+    requestId: string
+  }) => {
+
+
+    const url = `${this.baseUrl}gateway/v0.5/care-contexts/on-discover`
+    const body = {
+      "requestId": uuidv4(),
+      "timestamp": new Date().toISOString(),
+      "transactionId": config.transactionId,
+      "patient": {
+        "referenceNumber": config.patientReferenceNumber,
+        "display": config.patientDisplay,
+        "careContexts": config.careContexts,
+        "matchedBy": config.matchedBy
+      },
+      "error": {
+        "code": config.errCode,
+        "message": config.errMessage
+      },
+      "resp": {
+        "requestId": config.requestId
+      }
     }
 
-    onDiscovery = async(config:{
-        transactionId : string,
-        patientReferenceNumber : string;
-        patientDisplay : string;
-        careContexts : {"referenceNumber": string,"display": string}[],
-        matchedBy : string[]
-        errCode : string;
-        errMessage : string;
-         requestId: string
-    })=>{
+    await new Request().request({
+      "headers": this.headers(config.patientReferenceNumber), "method": "POST", "requestBody": body, "url": url
+    })
 
+    return body
 
-        const url = `${this.baseUrl}gateway/v0.5/care-contexts/on-discover`    
-   const body = {
-            "requestId": uuidv4(),
-            "timestamp": new Date().toISOString(),
-            "transactionId": config.transactionId,
-            "patient": {
-              "referenceNumber": config.patientReferenceNumber,
-              "display": config.patientDisplay,
-              "careContexts": config.careContexts,
-              "matchedBy":config.matchedBy
-            },
-            "error": {
-              "code": config.errCode,
-              "message": config.errMessage
-            },
-            "resp": {
-              "requestId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
-            }
-          }
-
-          await new Request().request({
-            "headers": this.headers(config.patientReferenceNumber), "method": "POST", "requestBody": body, "url": url
-        })
-
-        return body
-
-    }
+  }
 
 }
