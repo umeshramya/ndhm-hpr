e28756874bbafa22fa34ac330b728a4a76093549
pradyumnabilagi <umeshbilagi@gmail.com>
2022-02-20 19:22:27 +0530

    done

--- src/classes/discovery.ts
+++ src/classes/discovery.ts
@@ -1,50 +1,46 @@
 import Header from "./header";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import Request from "./request";
 
 export default class Discovery extends Header {
   constructor(_baseUrl: string, _accessToken: string) {
-    super(_baseUrl, _accessToken)
+    super(_baseUrl, _accessToken);
   }
 
-  onDiscovery = async (config: {
-    transactionId: string,
+  onDiscovery = (config: {
+    transactionId: string;
     patientReferenceNumber: string;
     patientDisplay: string;
-    careContexts: { "referenceNumber": string, "display": string }[],
-    matchedBy: string[]
+    careContexts: { referenceNumber: string; display: string }[];
+    matchedBy: string[];
     errCode: string;
     errMessage: string;
-    requestId: string
+    requestId: string;
   }) => {
-
-
-    const url = `${this.baseUrl}gateway/v0.5/care-contexts/on-discover`
+    const url = `${this.baseUrl}gateway/v0.5/care-contexts/on-discover`;
     const body = {
-      "requestId": uuidv4(),
-      "timestamp": new Date().toISOString(),
-      "transactionId": config.transactionId,
-      "patient": {
-        "referenceNumber": config.patientReferenceNumber,
-        "display": config.patientDisplay,
-        "careContexts": config.careContexts,
-        "matchedBy": config.matchedBy
+      requestId: uuidv4(),
+      timestamp: new Date().toISOString(),
+      transactionId: config.transactionId,
+      patient: {
+        referenceNumber: config.patientReferenceNumber,
+        display: config.patientDisplay,
+        careContexts: config.careContexts,
+        matchedBy: config.matchedBy,
       },
-      "error": {
-        "code": config.errCode,
-        "message": config.errMessage
+      error: {
+        code: config.errCode,
+        message: config.errMessage,
       },
-      "resp": {
-        "requestId": config.requestId
-      }
-    }
+      resp: {
+        requestId: config.requestId,
+      },
+    };
 
-    await new Request().request({
-      "headers": this.headers(config.patientReferenceNumber), "method": "POST", "requestBody": body, "url": url
-    })
+    // await new Request().request({
+    //   "headers": this.headers(config.patientReferenceNumber), "method": "POST", "requestBody": body, "url": url
+    // })
 
-    return body
-
-  }
-
+    return body;
+  };
 }
