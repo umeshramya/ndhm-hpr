f698969f3873f10c3bc87d2e332dac3f48001f97
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2022-05-13 08:03:38 +0530

    done

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import UserAuth from "./classes/userAuth";
 import Link from "./classes/link";
 import Discovery from "./classes/discovery";
 import ConsentFlow from "./classes/consent-flow";
+import Profile from "./classes/profile";
 
-export { Register, UserAuth, Link, Discovery, ConsentFlow };
+export { Register, UserAuth, Link, Discovery, ConsentFlow, Profile };
